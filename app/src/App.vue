dba2f377938f5d152cfcfcdb3414f16f72f64de2
potykion <potykion@gmail.com>
2019-02-17 19:23:48 +0300

    List all dependencies

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,14 +2,20 @@
     <div id="app">
         <v-stage :config="configKonva">
             <v-layer>
-                <v-circle :config="configCircle"></v-circle>
-                <v-text :config="configText"></v-text>
+                <Module v-for="(module, index) in dependencies"
+                        :x="100 + 200 * (index % 8)"
+                        :y="100 + 100 * Math.floor(index / 8)"
+                        :text="module.name">
+                </Module>
             </v-layer>
         </v-stage>
     </div>
 </template>
 
 <script>
+
+    import Module from "./components/Module";
+    import {mapState} from "vuex";
 
     export default {
         data() {
@@ -18,28 +24,11 @@
                     width: window.innerWidth,
                     height: window.innerHeight
                 },
-                configCircle: {
-                    // x, y - center of circle
-                    x: 100,
-                    y: 100,
-                    radius: 70,
-                    stroke: "black",
-                    strokeWidth: 2
-                },
-                configText: {
-                    // 70 = width
-                    x: 100 - 70 / 2,
-                    // 20 = fontSize
-                    y: 100 - 20 / 2,
-                    width: 70,
-                    fontSize: 20,
-                    text: "saas",
-                    align: "center"
-                }
             };
         },
         name: 'app',
-        components: {}
+        components: {Module},
+        computed: mapState(["dependencies"])
     }
 </script>
 
---
+++ app/src/components/Module.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-rect :config="configCircle"></v-rect>
+        <v-text :config="configText"></v-text>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Module",
+        props: ["x", "y", "text"],
+        data() {
+            return {
+                configCircle: {
+                    // x, y - center of circle
+                    x: this.x - 150 / 2,
+                    y: this.y - 50 / 2,
+                    width: 150,
+                    height: 50,
+                    stroke: "black",
+                    strokeWidth: 2,
+                    cornerRadius: 2
+                },
+                configText: {
+                    // 70 = width
+                    x: this.x - 150 / 2,
+                    // 20 = fontSize
+                    y: this.y - 10 / 2,
+                    width: 150,
+                    fontSize: 10,
+                    text: this.text,
+                    align: "center"
+                }
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
